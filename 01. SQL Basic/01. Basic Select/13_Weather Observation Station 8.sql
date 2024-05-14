0c2513a8c73ae25456033b70392ce580c426f08f
Do Le Phuong <145152948+lphngdo@users.noreply.github.com>
2024-05-14 18:03:04 +0700

    Weather Observation Station 8

---
+++ 01. SQL Basic/01. Basic Select/13_Weather Observation Station 8.sql
@@ -0,0 +1,16 @@
+SELECT DISTINCT CITY
+  FROM STATION
+ WHERE (
+        CITY LIKE 'a%' 
+     OR CITY LIKE 'o%' 
+     OR CITY LIKE 'e%'
+     OR CITY LIKE 'u%' 
+     OR CITY LIKE 'i%'
+  ) 
+   AND (
+        CITY LIKE '%a' 
+     OR CITY LIKE '%o' 
+     OR CITY LIKE '%e' 
+     OR CITY LIKE '%u' 
+     OR CITY LIKE '%i'
+  );
