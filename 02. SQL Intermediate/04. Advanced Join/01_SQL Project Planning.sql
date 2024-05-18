c39d563b5660ce460376bdb67a51a66827752fb7
Do Le Phuong <145152948+lphngdo@users.noreply.github.com>
2024-05-19 04:25:05 +0700

    SQL Project Planning

--- 02. SQL Intermediate/04. Advanced Join/01_SQL Project Planning.sql
+++ 02. SQL Intermediate/04. Advanced Join/01_SQL Project Planning.sql
@@ -1,1 +1,18 @@
-
+SELECT Start_Date, End_Date
+  FROM (
+    SELECT A.Start_Date, MIN(B.End_Date) AS End_Date
+      FROM
+        (
+            SELECT Start_Date
+            FROM Projects
+            WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)
+        ) AS A,
+        (
+            SELECT End_Date
+            FROM Projects
+            WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)
+        ) AS B
+     WHERE A.Start_Date < B.End_Date
+     GROUP BY A.Start_Date
+  ) AS P
+ ORDER BY DATEDIFF(millisecond, Start_Date, End_Date), Start_Date;
