9e398c9eddc715723e77aed323f59a6465b14bd4
Do Le Phuong <145152948+lphngdo@users.noreply.github.com>
2024-05-20 02:50:59 +0700

    Placements

--- 02. SQL Intermediate/04. Advanced Join/02_Placements.sql
+++ 02. SQL Intermediate/04. Advanced Join/02_Placements.sql
@@ -1,1 +1,10 @@
-
+SELECT S.Name
+  FROM Students AS S
+ INNER JOIN Friends AS F
+ ON S.ID = F.ID
+ INNER JOIN Packages AS PS
+ ON S.ID = PS.ID
+ INNER JOIN Packages AS PF
+ ON F.Friend_ID = PF.ID
+ WHERE PS.Salary < PF.Salary
+ ORDER BY PF.Salary;
