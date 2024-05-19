a57463eb3606991b8e3f17d08a5bae7836be07b1
Do Le Phuong <145152948+lphngdo@users.noreply.github.com>
2024-05-20 03:44:43 +0700

    Symmetric Pairs

--- 02. SQL Intermediate/04. Advanced Join/03_Symmetric Pairs.sql
+++ 02. SQL Intermediate/04. Advanced Join/03_Symmetric Pairs.sql
@@ -1,1 +1,15 @@
-
+SELECT F1.X, F1.Y
+  FROM Functions AS F1
+ INNER JOIN Functions AS F2
+ ON F1.X = F2.Y AND F2.X = F1.Y AND F1.X < F2.X
+ WHERE F1.X <> F1.Y
+ UNION
+SELECT X, Y
+  FROM Functions AS F1
+ WHERE X = Y 
+   AND (
+    SELECT COUNT(*)
+      FROM Functions AS F2
+     WHERE F2.X = F1.X AND F2.Y = F1.X
+   ) > 1
+ ORDER BY X;
