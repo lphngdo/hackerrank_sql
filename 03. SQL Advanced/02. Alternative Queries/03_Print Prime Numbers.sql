b6f70308dd34f504d7604c02f746d573dc18279c
Do Le Phuong <145152948+lphngdo@users.noreply.github.com>
2024-05-20 15:11:10 +0700

    Print Prime Numbers

--- 03. SQL Advanced/02. Alternative Queries/03_Print Prime Numbers.sql
+++ 03. SQL Advanced/02. Alternative Queries/03_Print Prime Numbers.sql
@@ -1,1 +1,34 @@
+DECLARE @output NVARCHAR(MAX) = '';
+DECLARE @n INT;
+DECLARE @counter BIT;
+DECLARE @a INT;
+DECLARE @b INT;
 
+SET @n = 2;
+
+WHILE @n <= 1000
+BEGIN
+    SET @counter = 0;
+    SET @a = FLOOR(@n / 2);
+    SET @b = 2;
+
+    WHILE @b <= @a
+    BEGIN
+        IF (@n % @b = 0)
+        BEGIN
+            SET @counter = 1;
+        END
+        SET @b = @b + 1;
+    END
+
+    IF (@counter = 0)
+    BEGIN
+        SET @output = @output + CAST(@n AS NVARCHAR) + '&';
+    END
+
+    SET @n = @n + 1;
+END
+
+SET @output = LEFT(@output, LEN(@output) - 1);
+
+PRINT @output;
