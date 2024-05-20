49bd8e029df492d237a58fb1c25d131f589a53c6
Do Le Phuong <145152948+lphngdo@users.noreply.github.com>
2024-05-20 15:03:41 +0700

    Draw The Triangle 1

--- 03. SQL Advanced/02. Alternative Queries/01_Draw The Triangle 1.sql
+++ 03. SQL Advanced/02. Alternative Queries/01_Draw The Triangle 1.sql
@@ -1,1 +1,13 @@
+DECLARE @temp_num INT;
+SET @temp_num = 21;
 
+WITH CTE_Numbers AS (
+    SELECT 1 AS n
+    UNION ALL
+    SELECT n + 1
+      FROM CTE_Numbers
+     WHERE n < @temp_num
+)
+SELECT REPLICATE('* ', @temp_num - n)
+  FROM CTE_Numbers
+OPTION (MAXRECURSION 0);
